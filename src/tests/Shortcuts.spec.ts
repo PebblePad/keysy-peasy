bda74ab98e7311310155147f2beca77d404764f5
Chris Rowe <chris@pebblelearning.co.uk>
2020-08-26 16:37:05 +0100

    Add undefined check for event.key and some additional unit tests

--- src/tests/Shortcuts.spec.ts
+++ src/tests/Shortcuts.spec.ts
@@ -1,11 +1,10 @@
 import { Shortcuts } from "../classes/Shortcuts";
-import { KeysyPeasyError } from "..";
+import { KeysyPeasyError } from "../classes/KeasyPeasyError";
 
-Object.defineProperty(document, "addEventListener", (event: string, callback: () => void) => {});
-
-describe('When registering events', () => {
-    const shortcuts = new Shortcuts(document.documentElement);
-    const callback = () => {};
+describe('When using keysy peasy', () => {
+    jest.useFakeTimers();
+    let shortcuts: Shortcuts;
+    const callback = jest.fn();
 
     const domainTestId = "domain test";
     const firstTestId = "first test";
@@ -35,37 +34,81 @@ describe('When registering events', () => {
         altKey:false
     };
 
-    shortcuts.register(firstTestId, [exampleShortcut1, exampleShortcut2]);
-    shortcuts.register(secondTestId, [exampleShortcut3]);
+    beforeEach(() => {
+        shortcuts = new Shortcuts(document.documentElement);
+    })
 
-    test('it should correctly initialise the shortcut', () => {
-        shortcuts.register(domainTestId, [exampleShortcut]);
-        const shortcut = shortcuts.getHandlers()[exampleShortcut.key];
-        expect(shortcut.key).toEqual(exampleShortcut.key);
-        expect(shortcut.contextId).toEqual(domainTestId);
-        expect(shortcut.altKey).toEqual(exampleShortcut.altKey);
-        expect(shortcut.callback).toEqual(exampleShortcut.callback);
-    });
+    afterEach(() => {
+        callback.mockReset();
+        shortcuts.destroy();
+    })
 
-    test("it should not let you register two of the same shortcut", () => {
-        expect(() => shortcuts.register("random-id", [exampleShortcut]))
-            .toThrow(new KeysyPeasyError("Duplicate shortcut", exampleShortcut));
-    });
+    describe("and registering events", () => {
+        beforeEach(() => {
+            shortcuts.register(firstTestId, [exampleShortcut1, exampleShortcut2]);
+            shortcuts.register(secondTestId, [exampleShortcut3]);
+        })
 
-    test('it should correctly register events with different Ids', () => {
-        const shortcutMap = shortcuts.getHandlers();
-        expect(shortcutMap[exampleShortcut1.key]).toBeDefined();
-        expect(shortcutMap[exampleShortcut2.key]).toBeDefined();
-        expect(shortcutMap[exampleShortcut3.key]).toBeDefined();
-    });
+        test('it should correctly initialise the shortcut', () => {
+            shortcuts.register(domainTestId, [exampleShortcut]);
+            const shortcut = shortcuts.getHandlers()[exampleShortcut.key];
+            expect(shortcut.key).toEqual(exampleShortcut.key);
+            expect(shortcut.contextId).toEqual(domainTestId);
+            expect(shortcut.altKey).toEqual(exampleShortcut.altKey);
+            expect(shortcut.callback).toEqual(exampleShortcut.callback);
+        });
 
-    describe('and an event is removed', () => {
-        test('it should remove events by domain id', () => {
-            shortcuts.remove(firstTestId);
+        test("it should not let you register two of the same shortcut", () => {
+            shortcuts.register(domainTestId, [exampleShortcut]);
+            expect(() => shortcuts.register("random-id", [exampleShortcut]))
+                .toThrow(new KeysyPeasyError("Duplicate shortcut", exampleShortcut));
+        });
+
+        test('it should correctly register events with different Ids', () => {
             const shortcutMap = shortcuts.getHandlers();
-            expect(shortcutMap[exampleShortcut1.key]).toBeUndefined();
-            expect(shortcutMap[exampleShortcut2.key]).toBeUndefined();
+            expect(shortcutMap[exampleShortcut1.key]).toBeDefined();
+            expect(shortcutMap[exampleShortcut2.key]).toBeDefined();
             expect(shortcutMap[exampleShortcut3.key]).toBeDefined();
+        });
+
+        describe('and an event is removed', () => {
+            test('it should remove events by domain id', () => {
+                shortcuts.remove(firstTestId);
+                const shortcutMap = shortcuts.getHandlers();
+                expect(shortcutMap[exampleShortcut1.key]).toBeUndefined();
+                expect(shortcutMap[exampleShortcut2.key]).toBeUndefined();
+                expect(shortcutMap[exampleShortcut3.key]).toBeDefined();
+            });
+        });
+    });
+
+    describe("and an event occurs", () => {
+        beforeEach(() => {
+            shortcuts.register(secondTestId, [exampleShortcut3]);
+        });
+
+        describe("and a shortcut is registered for the events key", () => {
+            test("it should trigger the callback", () => {
+                document.documentElement.dispatchEvent(new KeyboardEvent("keydown", {"key": "p", bubbles: true}));
+                jest.runAllTimers();
+                expect(callback).toBeCalledTimes(1)
+            });
+        });
+
+        describe("and no shortcut is registered for the events key", () => {
+            test("it should trigger the callback", () => {
+                document.documentElement.dispatchEvent(new KeyboardEvent("keydown", {"key": "g", bubbles: true}));
+                jest.runAllTimers();
+                expect(callback).not.toBeCalled();
+            });
+        });
+
+        describe("and the key is undefined", () => {
+            test("it should not run the callback", () => {
+                document.documentElement.dispatchEvent(new KeyboardEvent("keydown"));
+                jest.runAllTimers();
+                expect(callback).not.toBeCalled();
+            });
         });
     });
 });
